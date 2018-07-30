5fc55cf6c8340cdbaff3eb7ec795e3fc99e4d825
cnzmg <cnzmg@git.com>
2018-07-30 11:40:56 +0800

    readme --cnzmg

--- js/base.js
+++ js/base.js
@@ -273,7 +273,7 @@ jzm.payMoney = function(){
                     jzm.Error(reg);
                 };
                 if (reg.statusCode.status == 1012){
-                    alert(reg.statusCode.msg);
+                    alert("支付完成，使用【兑换码】到机器上兑换产品吧！");
                     window.location.href = "../html/my_order.html";
                 }else{
 					jzm.Error(reg);
@@ -310,6 +310,7 @@ jzm.payMoney = function(){
                    },
                    function(res){
 	                   if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+	                   		alert("支付完成，使用【兑换码】到机器上兑换产品吧！");
 	                    	window.location.href = "../html/my_order.html";
 	                   } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                    }
